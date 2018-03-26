5257b108d1509bb30f5b358bfe98a7a96b26bcfc
Max <maximilian.paulsen@gmail.com>
2018-03-26 16:30:06 -0700

    added codemirror editor

--- Simulator/src/index.js
+++ Simulator/src/index.js
@@ -1,3 +1,10 @@
 import Computer from "./Computer.js";
 
 window.computer = new Computer();
+
+
+var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("editor"),
+{
+    lineNumbers: true,
+    mode: "gas"
+  });
